73f24ec9646bc566d2cc4a2f3620d6d598014b8c
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-02-04 16:23:45 +0700

    try some theme config

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,12 +1,53 @@
 -- handle for nightfox
 --
 return {
+    -- {
+    --     "wtfox/jellybeans.nvim",
+    --     priority = 1000,
+    --     config = function()
+    --         require("jellybeans").setup()
+    --         vim.cmd.colorscheme("jellybeans")
+    --     end,
+    -- },
     {
-        "wtfox/jellybeans.nvim",
+        "dgox16/oldworld.nvim",
+        lazy = false,
         priority = 1000,
         config = function()
-            require("jellybeans").setup()
-            vim.cmd.colorscheme("jellybeans")
+            require("oldworld").setup({
+                terminal_colors = true, -- enable terminal colors
+                variant = "oled", -- default, oled, cooler
+                styles = { -- You can pass the style using the format: style = true
+                    comments = {}, -- style for comments
+                    keywords = {}, -- style for keywords
+                    identifiers = {}, -- style for identifiers
+                    functions = {}, -- style for functions
+                    variables = {}, -- style for variables
+                    booleans = {}, -- style for booleans
+                },
+                integrations = { -- You can disable/enable integrations
+                    alpha = true,
+                    cmp = true,
+                    flash = true,
+                    gitsigns = true,
+                    hop = false,
+                    indent_blankline = true,
+                    lazy = true,
+                    lsp = true,
+                    markdown = true,
+                    mason = true,
+                    navic = false,
+                    neo_tree = false,
+                    neorg = false,
+                    noice = true,
+                    notify = true,
+                    rainbow_delimiters = true,
+                    telescope = true,
+                    treesitter = true,
+                },
+                highlight_overrides = {},
+            })
+            vim.cmd.colorscheme("oldworld")
         end,
     },
 }
