2421d002c5d6d01e317eea8115ef8bd0e432f2bb
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-02-09 23:27:11 +0700

    refactoring lsp

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,9 +1,35 @@
 return {
-    {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-        },
+    'neovim/nvim-lspconfig',
+    -- Setting up LSP servers
+    dependencies =  {
+        require('plugins.lsp.servers'),
+        --for omnisharp support
+        "Hoffs/omnisharp-extended-lsp.nvim",
     },
-    require("plugins.lsp.lspconfig"),
+
+    config = function()
+        --preload before setting up lspconfig
+        require("plugins.lsp.preload")
+
+        -- This is where you enable features that only work
+        -- if there is a language server active in the file
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = {buffer = event.buf}
+
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua require("omnisharp_extended").lsp_definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua require("omnisharp_extended").lsp_implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua require("omnisharp_extended").lsp_type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua require("omnisharp_extended").lsp_references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            end,
+        })
+
+    end,
 }
---
+++ lua/plugins/lsp/preload.lua
@@ -0,0 +1,12 @@
+-- Reserve a space in the gutter
+-- This will avoid an annoying layout shift in the screen
+vim.opt.signcolumn = 'yes'
+
+-- Add cmp_nvim_lsp capabilities settings to lspconfig
+-- This should be executed before you configure any language server
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lspconfig_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
