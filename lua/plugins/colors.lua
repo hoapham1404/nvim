eac85fb1ad57066aae759f6513a6042027703234
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-03-28 14:20:36 +0700

    update

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,53 +1,10 @@
--- handle for nightfox
---
 return {
-    -- {
-    --     "wtfox/jellybeans.nvim",
-    --     priority = 1000,
-    --     config = function()
-    --         require("jellybeans").setup()
-    --         vim.cmd.colorscheme("jellybeans")
-    --     end,
-    -- },
     {
-        "dgox16/oldworld.nvim",
-        lazy = false,
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
         config = function()
-            require("oldworld").setup({
-                terminal_colors = true, -- enable terminal colors
-                variant = "oled", -- default, oled, cooler
-                styles = { -- You can pass the style using the format: style = true
-                    comments = {}, -- style for comments
-                    keywords = {}, -- style for keywords
-                    identifiers = {}, -- style for identifiers
-                    functions = {}, -- style for functions
-                    variables = {}, -- style for variables
-                    booleans = {}, -- style for booleans
-                },
-                integrations = { -- You can disable/enable integrations
-                    alpha = true,
-                    cmp = true,
-                    flash = true,
-                    gitsigns = true,
-                    hop = false,
-                    indent_blankline = true,
-                    lazy = true,
-                    lsp = true,
-                    markdown = true,
-                    mason = true,
-                    navic = false,
-                    neo_tree = false,
-                    neorg = false,
-                    noice = true,
-                    notify = true,
-                    rainbow_delimiters = true,
-                    telescope = true,
-                    treesitter = true,
-                },
-                highlight_overrides = {},
-            })
-            vim.cmd.colorscheme("oldworld")
+            vim.cmd.colorscheme("catppuccin")
         end,
     },
 }
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,13 +4,15 @@ return {
     config = function()
         require("mini.diff").setup()
         require("mini.pairs").setup()
-        require('mini.surround').setup({
+        require("mini.surround").setup({
             custom_surroundings = {
                 T = {
-                    input = { '<(%w+)[^<>]->.-</%1>', '^<()%w+().*</()%w+()>$' },
+                    input = { "<(%w+)[^<>]->.-</%1>", "^<()%w+().*</()%w+()>$" },
                     output = function()
-                        local tag_name = MiniSurround.user_input('Tag name')
-                        if tag_name == nil then return nil end
+                        local tag_name = MiniSurround.user_input("Tag name")
+                        if tag_name == nil then
+                            return nil
+                        end
                         return { left = tag_name, right = tag_name }
                     end,
                 },
@@ -21,7 +23,7 @@ return {
         local hipatterns = require("mini.hipatterns")
         hipatterns.setup({
             highlighters = {
-                hex_color = hipatterns.gen_highlighter.hex_color(), -- Highlight hex color strings (`#rrggbb` like `#123456`)) using that color
+                hex_color = hipatterns.gen_highlighter.hex_color(), -- Highlight hex color strings (`#rrggbb` like `#123456`)) using that color,
             },
         })
 
