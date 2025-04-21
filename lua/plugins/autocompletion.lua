34e571046ce56585e8e390b7c205208075484ac3
hoapham <phamquanghoa1404@gmail.com>
2025-04-21 21:08:40 +0700

    update

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -41,11 +41,7 @@ return {
             preselect = "item",
             completion = {
                 compleopt = "menu,menuone,noinsert"
-            },
-            window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-            },
+            }
         })
     end,
 }
