56a64e47823074d66ed5ed2534ac0dc465f7a50e
hoapham <phamquanghoa1404@gmail.com>
2025-06-01 12:45:33 +0700

    combine lsp config together

--- lua/init_lazy.lua
+++ lua/init_lazy.lua
@@ -19,4 +19,5 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     { import = "plugins" },
+    { import = "plugins.lsp"}
 })
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -6,8 +6,11 @@ return {
         config = function()
             require("catppuccin").setup({
                 transparent_background = true,
+
             })
             vim.cmd.colorscheme("catppuccin")
+            vim.api.nvim_set_hl(0, "LineNr", { fg = "#7f849c", bg = "NONE" })       -- normal line numbers
+            vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#f38ba8", bold = true }) -- current line number
         end,
     },
 }
