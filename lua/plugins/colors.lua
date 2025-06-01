187b44bb961d91db96a4fe28afa6d01ad4ba53e4
hoapham <phamquanghoa1404@gmail.com>
2025-06-01 13:05:16 +0700

    fix syntax lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,16 +1,14 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                transparent_background = true,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            transparent_background = true,
 
-            })
-            vim.cmd.colorscheme("catppuccin")
-            vim.api.nvim_set_hl(0, "LineNr", { fg = "#7f849c", bg = "NONE" })       -- normal line numbers
-            vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#f38ba8", bold = true }) -- current line number
-        end,
-    },
+        })
+        vim.cmd.colorscheme("catppuccin")
+        vim.api.nvim_set_hl(0, "LineNr", { fg = "#7f849c", bg = "NONE" })       -- normal line numbers
+        vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#f38ba8", bold = true }) -- current line number
+    end
 }
